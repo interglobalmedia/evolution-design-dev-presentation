fd21b762a718705f0e002e23176c29502d0af79f
interglobalmedia <interglobalmedia@gmail.com>
2019-01-23 13:39:12 -0500

    Updates

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.8f738a754abdf07997afcfb9626ea613.js"
+  "precache-manifest.849e164716da2fe917a005be1a4b6089.js"
 );
 
 /**
